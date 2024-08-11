098c065c69a7108363bc416f13ee59118f5d8394
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2024-08-11 02:39:02 +0200

    Update enemies creator

--- resources/views/gm-main.blade.php
+++ resources/views/gm-main.blade.php
@@ -1,9 +1,9 @@
 @extends('base')
 @section('content')
-    <div class="pl-16">
+    <div class="pl-16 flex gap-10">
 
         <div>
-
+            <!-- Dices roll -->
             <fieldset
                 class="border border-black flex w-64 cursor-pointer select-none hover:scale-110 transition transform"
                 onclick="roll20()">
@@ -18,7 +18,6 @@
                     </tr>
                 </table>
             </fieldset>
-
             <fieldset
                 class="border border-black flex w-64 cursor-pointer select-none hover:scale-110 transition transform"
                 onclick="roll6()">
@@ -61,11 +60,40 @@
                     </tr>
                 </table>
             </fieldset>
+            <!-- End dices roll -->
+        </div>
+        <div>
+            <!-- Ennemies -->
+            <fieldset
+                class="border border-black flex">
+                <legend>
+                    <div class="px-2">Ennemies</div>
+                </legend>
+                <table>
+                    <tr>
+                        <td class="flex gap-4">
+                            <button onclick="createEnemy(0, 1, 'T1')">Tier1</button>
+                            <button onclick="createEnemy(8, 1.5, 'T2')">Tier2</button>
+                            <button onclick="createEnemy(12, 2, 'T3')">Tier3</button>
+                            <button onclick="createEnemy(16, 2.6, 'T4')">Tier4</button>
+                            <button onclick="createEnemy(24, 3, 'T5')">Tier5</button>
+                            <button onclick="createEnemy(30, 4, 'B1')">Boss1</button>
+                            <button onclick="createEnemy(40, 6, 'B2')">Boss2</button>
+                            <button onclick="createEnemy(50, 8, 'B3')">Boss3</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td id="enemies">
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>
 
         </div>
     </div>
 
     <script>
+        // Dices roll
         function roll20() {
             let roll = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
             document.getElementById("roll20").textContent = "Roll20 : " + roll.toString();
@@ -83,6 +111,7 @@
             document.getElementById('roll20-hist').prepend(element);
 
         }
+
         function roll6() {
             let roll = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
             document.getElementById("roll6").textContent = "Roll6 : " + roll.toString();
@@ -98,6 +127,7 @@
             document.getElementById('roll6-hist').prepend(element);
 
         }
+
         function roll4() {
             let roll = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
             document.getElementById("roll4").textContent = "Roll4 : " + roll.toString();
@@ -113,6 +143,7 @@
             document.getElementById('roll4-hist').prepend(element);
 
         }
+
         function roll2() {
             let roll = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
             document.getElementById("roll2").textContent = "Roll2 : " + roll.toString();
@@ -126,6 +157,95 @@
             }
 
             document.getElementById('roll2-hist').prepend(element);
+
+        }
+
+        // End ices roll
+
+        function createEnemy(type, hpBoost, name) {
+
+            let addPower = 15 + type;
+
+            let strength = 6;
+            let dexterity = 6;
+            let constitution = 6;
+            let intelligence = 6;
+            let wisdom = 6;
+            let charisma = 6;
+
+            while (addPower > 0) {
+                let addStat = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+
+                switch (addStat) {
+                    case 1:
+                        strength += 1;
+                        break
+                    case 2:
+                        dexterity += 1;
+                        break
+                    case 3:
+                        constitution += 1;
+                        break
+                    case 4:
+                        intelligence += 1;
+                        break
+                    case 5:
+                        wisdom += 1;
+                        break
+                    case 6:
+                        charisma += 1;
+                        break
+                }
+                addPower -= 1;
+            }
+
+            let health = Math.floor(constitution * hpBoost);
+            console.log(strength + "-" + dexterity + "-" + constitution + "-" + intelligence + "-" + wisdom + "-" + charisma + "-" + health)
+
+            var fieldset = document.createElement("fieldset");
+            fieldset.classList.add("border", "border-black", "m-2");
+            var legend = document.createElement("legend");
+            legend.classList.add("px-2");
+            legend.textContent = name;
+            var removeDiv = document.createElement("span");
+            removeDiv.classList.add("ml-2", "cursor-pointer", "text-red-500")
+            removeDiv.textContent = "SUPPRIMER";
+            removeDiv.onclick = function() {
+                fieldset.remove();
+            };
+            var table = document.createElement("table")
+            var tr = document.createElement("tr")
+            var td = document.createElement("td")
+            td.classList.add("grid", "grid-cols-6", "gap-2")
+            var docStrength = document.createElement("div")
+            docStrength.textContent = "FOR : " + strength.toString();
+            var docDexterity = document.createElement("div")
+            docDexterity.textContent = "DEX : " + dexterity.toString();
+            var docConstitution = document.createElement("div")
+            docConstitution.textContent = "CON : " + constitution.toString();
+            var docIntelligence = document.createElement("div")
+            docIntelligence.textContent = "INT : " + intelligence.toString();
+            var docWisdom = document.createElement("div")
+            docWisdom.textContent = "SAG : " + wisdom.toString();
+            var docCharisma = document.createElement("div")
+            docCharisma.textContent = "CHA : " + charisma.toString();
+            var docHealth = document.createElement("div")
+            docHealth.textContent = "PDV : " + health.toString();
+
+            fieldset.appendChild(legend);
+            legend.appendChild(removeDiv);
+            fieldset.appendChild(table);
+            table.appendChild(tr);
+            tr.appendChild(td);
+            td.appendChild(docStrength);
+            td.appendChild(docDexterity);
+            td.appendChild(docConstitution);
+            td.appendChild(docIntelligence);
+            td.appendChild(docWisdom);
+            td.appendChild(docCharisma);
+            td.appendChild(docHealth);
+
+            document.getElementById('enemies').appendChild(fieldset);
 
         }
     </script>
